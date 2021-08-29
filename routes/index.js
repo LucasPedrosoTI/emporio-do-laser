53adb682ee918664130c3222b07d44a6923e4504
jr-quint <juniorquintilhano@hotmail.com>
2021-08-29 16:13:05 -0300

    adicionado novas rotas

--- routes/index.js
+++ routes/index.js
@@ -11,13 +11,23 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Empório do Laser' });
 });
 router.get('/sobre', function (req, res, next) {
-  res.render('about-us', { title: 'Sobre Nós' });
+  res.render('navbar/about-us', { title: 'Sobre Nós' });
 });
 router.get('/sac', function (req, res, next) {
-  res.render('sac', { title: 'Contato/SAC' });
+  res.render('navbar/sac', { title: 'Contato/SAC' });
 });
 router.get('/minha-conta', function (req, res, next) {
-  res.render('my-account', { title: 'Minha Conta' });
+  res.render('navbar/my-account', { title: 'Minha Conta' });
 });
 
+/* GET nav bar Produtos */
+router.get('/produtos/cakeboards', function (req, res, next) {
+  res.render('navbar/produtos/cakeboards', { title: 'Produtos: Cakeboards' });
+});
+router.get('/produtos/caixas-cenario', function (req, res, next) {
+  res.render('navbar/produtos/caixas-cenario', { title: 'Produtos: Cenário' });
+});
+router.get('/produtos/topo-bolos', function (req, res, next) {
+  res.render('navbar/produtos/topo-bolos', { title: 'Produtos: Topo de bolos' });
+});
 module.exports = router;
