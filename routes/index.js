8ace3c7213a147ffc6f1b3415b3beb61c9f06b17
jr-quint <juniorquintilhano@hotmail.com>
2021-08-29 15:51:51 -0300

    rotas do menu adicionadas

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Empório do Laser' });
 });
 
+/* GET nav bar */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Empório do Laser' });
+});
+router.get('/sobre', function (req, res, next) {
+  res.render('about-us', { title: 'Sobre Nós' });
+});
+router.get('/sac', function (req, res, next) {
+  res.render('sac', { title: 'Contato/SAC' });
+});
+router.get('/minha-conta', function (req, res, next) {
+  res.render('my-account', { title: 'Minha Conta' });
+});
+
 module.exports = router;
