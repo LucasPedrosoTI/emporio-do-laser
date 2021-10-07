22e71d70bd3e6b9b12240b179580af343cf19e69
LucasPedrosoTI <Lucas.PedrosoTI@gmail.com>
2021-10-06 21:49:43 -0300

    Configuração variaveis de ambiente

--- config/database.js
+++ config/database.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
     dialect: 'mysql',
-    username: 'root',
-    password: 'root',
-    database: 'emporio-do-laser',
-    host: '127.0.0.1',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
   },
   production: {
     username: 'root',
