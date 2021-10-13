a717f5ecee4debfd908706890dd1a021f9aa4640
jr-quint <juniorquintilhano@hotmail.com>
2021-10-13 02:46:58 -0300

    Adiciona funcionalidades nos botões da página Carrinho

--- controllers/carrinhoController.js
+++ controllers/carrinhoController.js
@@ -5,9 +5,7 @@ module.exports = {
 
     listar: (req, res) => {
 
-        req.session.carrinho = [];
-
-        if(req.session.carrinho){
+        if(!req.session.carrinho){
             req.session.carrinho = [
 
                 {id: 1, qtd: 1, nomeProduto: "Aloha", descricao: "Topo bolo Aloha", valorUnit: 5, imagem: "/img/topo1.png"},
@@ -27,13 +25,41 @@ module.exports = {
 
     excluir: (req, res) => {
 
-
+        let sessao = req.session.carrinho;
+        for(let i = 0; i < sessao.length; i++) {
+            if(sessao[i].id == req.body.id){
+                req.session.carrinho.splice(i, 1);
+                res.json(req.session.carrinho);
+            }
+        }
 
     },
     
     qtd: (req, res) => {
 
+        if (req.session.carrinho) {
+            if (req.body.id
+            && !isNaN(req.body.id)
+            && req.body.qtd
+            && !isNaN(req.body.qtd)
+            && req.body.qtd > 0) {
+                
+                let id = req.body.id;
+                let qtd = req.body.qtd;
+                let sessao = req.session.carrinho;
 
+                for(let i = 0; i < sessao.length; i++) {
+                    if(sessao[i].id == req.body.id){
+                        req.session.carrinho[i].qtd = qtd;
+
+                        res.json(req.session.carrinho);
+                    }
+                }
+            }
+        
+        } else {
+            res.json();
+        }
         
     },
 
