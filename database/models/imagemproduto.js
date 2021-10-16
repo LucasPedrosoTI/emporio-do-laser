678ad01f2c3ebd920b2faabbb36dc522fde5d425
LucasPedrosoTI <Lucas.PedrosoTI@gmail.com>
2021-10-16 20:25:06 -0300

    ajuste nome tabela imagem-produto

--- database/models/imagemproduto.js
+++ database/models/imagemproduto.js
@@ -16,7 +16,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: 'ImagemProduto',
-      tableName: 'Imagems_Produtos',
+      tableName: 'Imagens_Produtos',
     }
   );
   return ImagemProduto;
