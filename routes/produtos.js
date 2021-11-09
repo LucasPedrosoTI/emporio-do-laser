9d751aa23892b0f3f0665c977751c65a7caf696a
Mauricio Sousa <mauricio_sousa20@hotmail.com>
2021-11-09 16:24:05 -0300

    Criação tela de compra de produto

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 const produtoController = require('../controllers/produtoController');
 /* GET nav bar Produtos */
 router.get('/', produtoController.renderProdutos);
+
+router.get('/produto', function (req, res, next) {
+    res.render('produto', { title: 'Empório do Laser - Sobre Nós' });
+  });
 
 module.exports = router;
