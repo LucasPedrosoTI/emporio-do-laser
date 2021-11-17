8e0c4301efa95d8f12b6af5560a2580c195fa2dc
jr-quint <juniorquintilhano@hotmail.com>
2021-11-17 14:39:13 -0300

    corrige authAdmin, elimina erro de página não encontrada

--- middlewares/authAdmin.js
+++ middlewares/authAdmin.js
@@ -1,8 +1,12 @@
 function authAdmin(req, res, next) {
-  if (req.session.usuario.admin) {
-    return next();
+  if (req.session.usuario) {
+    if (req.session.usuario.admin) {
+      return next();
+    } else {
+      res.redirect('/minha-conta')
+    }
   } else {
-    res.redirect('/minha-conta');
+    res.redirect('/identifique-se');
   }
 }
 
